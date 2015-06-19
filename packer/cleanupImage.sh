0d2d17af850f79c89def7770a2dfd1e180a8a7cc
Steffen Bleul <blacklabelops@itbleul.de>
2015-06-19 12:36:28 +0200

    Moved cleanup routine to Vagrantfile due to errors in build.

--- packer/cleanupImage.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-# These were only needed for building VMware/Virtualbox extensions:
-yum -y clean all
-rm -rf VBoxGuestAdditions_*.iso VBoxGuestAdditions_*.iso.?
-d if=/dev/zero of=/EMPTY bs=1M
-rm -f /EMPTY
-sync
---
+++ scripts/cleanup.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+# These were only needed for building VMware/Virtualbox extensions:
+sudo yum -y clean all
