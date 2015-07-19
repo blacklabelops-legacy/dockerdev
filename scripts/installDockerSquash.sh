97f952943191dd4a3f5b7c3dd16670024408e40e
Steffen Bleul <blacklabelops@itbleul.de>
2015-07-19 20:28:01 +0200

    Added docker squash tool.

--- scripts/installDockerSquash.sh
+++ scripts/installDockerSquash.sh
@@ -2,4 +2,4 @@
 
 # Installation of Docker Squash
 wget https://github.com/jwilder/docker-squash/releases/download/v0.1.0/docker-squash-linux-amd64-v0.1.0.tar.gz
-sudo tar -C /usr/local/bin -xzvf docker-squash-linux-amd64-v0.1.0.tar.gz
+sudo tar -C /usr/bin -xzvf docker-squash-linux-amd64-v0.1.0.tar.gz
